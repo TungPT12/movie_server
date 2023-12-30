3377e7a0640ffea4f1fb5f2dfc6a94cea04b6af8
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-30 23:45:22 +0700

    change api

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://TungPT:BoyPham1204@mongodb.y8qiteb.mongodb.net/movie_website?retryWrites=true&w=majority";
 
-const connectMongoose = (cb) => {
-    mongoose.connect(uri).then(() => {
-        cb();
-        console.log('Connected!')
+const connectMongoose = (callback) => {
+    mongoose.connect(uri).then((client) => {
+        callback();
+        // console.log('Connected!')
     }).catch((error) => {
         console.log(error);
         throw "Error connect mongoose";
--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const port = 5000;
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:3000',
-        'http://localhost:3001',],
+        'http://localhost:3001', "https://movie-trailer-client.netlify.app",],
     credentials: true
 }));
 
