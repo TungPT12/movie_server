71a90e2e6d00504380b83de2e5454805523264d2
TungPT12 <mylifeundermycontrol@gmail.com>
2024-07-28 15:26:08 +0700

    Update mongoose.js

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://TungPT:BoyPham1204@mongodb.y8qiteb.mongodb.net/movie_website?retryWrites=true&w=majority";
+const uri = "mongodb+srv://TungPT:TungPT12345@mongodb.y8qiteb.mongodb.net/movie_website?retryWrites=true&w=majority";
 
 const connectMongoose = (callback) => {
     mongoose.connect(uri).then((client) => {
