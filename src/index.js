c8eec46e45bd1bc2322138ca36a43b9f3f548b38
TungPT12 <mylifeundermycontrol@gmail.com>
2024-08-12 21:35:15 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const session = require('express-session');
 const connectMongoose = require('./configs/mongoose')
-const MongoDBStore = require('connect-mongodb-session')(session);
+// const MongoDBStore = require('connect-mongodb-session')(session);
 const socketIO = require('./utils/socket');
 const app = express()
 
 //admin
-const authnRouter = require('./router/admin/authn');
-const movieRouter = require('./router/admin/movies')
+// const authnRouter = require('./router/admin/authn');
+// const movieRouter = require('./router/admin/movies')
 
 //client
 const movieRoutes = require('./router/client/movie')
@@ -27,27 +27,27 @@ app.use(cors({
     credentials: true
 }));
 
-const store = MongoDBStore({
-    uri: 'mongodb+srv://TungPT:BoyPham1204@mongodb.y8qiteb.mongodb.net/movie_website?retryWrites=true&w=majority',
-    collection: 'session'
-})
+// const store = MongoDBStore({
+//     uri: 'mongodb+srv://TungPT:BoyPham1204@mongodb.y8qiteb.mongodb.net/movie_website?retryWrites=true&w=majority',
+//     collection: 'session'
+// })
 
-app.use(session({
-    secret: "keyword",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    }
-}))
+// app.use(session({
+//     secret: "keyword",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store,
+//     cookie: {
+//         maxAge: 1000 * 60 * 60 * 24 * 7,
+//     }
+// }))
 
 
 
 
 //admin
-app.use("/api/admin", authnRouter);
-app.use("/api/admin", movieRouter);
+// app.use("/api/admin", authnRouter);
+// app.use("/api/admin", movieRouter);
 app.use(movieRoutes)
 app.use(orderRoutes)
 
@@ -69,15 +69,15 @@ const runningServer = () => {
         console.log(`Running on http://localhost:${port}`)
     })
 
-    socketIO.initIO(server, {
-        cors: {
-            origin: '*',
-            method: ['POST', 'GET']
-        }
-    });
+    // socketIO.initIO(server, {
+    //     cors: {
+    //         origin: '*',
+    //         method: ['POST', 'GET']
+    //     }
+    // });
 
-    socketIO.getIO().on('connection', (socket) => {
-    })
+    // socketIO.getIO().on('connection', (socket) => {
+    // })
 }
-
-connectMongoose(runningServer);
+runningServer()
+// connectMongoose(runningServer);
